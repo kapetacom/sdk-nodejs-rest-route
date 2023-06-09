48afe181f6110a1ce89f48f2049d864208efcde5
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-09 13:36:22 +0200

    feat: Rewrote to TS (#1)

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+    extends: ['@kapeta/eslint-config'],
+    env: {
+        node: true,
+    },
+    rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
+        'no-param-reassign': 'off',
+        '@typescript-eslint/require-await': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/no-misused-promises': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        'no-ex-assign': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/ban-types': 'off',
+    },
+    parserOptions: {
+        project: `${__dirname}/tsconfig.json`,
+        tsconfigRootDir: __dirname,
+    },
+};
